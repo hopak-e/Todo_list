6cf9f478498a5c02013eabf80461f78e1a3b56c4
hopak-e <a328028@gmail.com>
2023-01-27 23:05:43 +0900

    ✨ 로그인 한 유저정보 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+import Calendar from 'pages/Calendar'
+import Memo from 'pages/Memo'
+import SignInPage from 'pages/SignInPage'
+import SignUpPage from 'pages/SignUpPage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const userData = user
+            console.log(userData)
+        } else {
+            console.log('hihi')
+        }
+    })
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Calendar />} />
+                <Route path="/memo" element={<Memo />} />
+                <Route path="/signin" element={<SignInPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
